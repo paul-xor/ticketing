0ca8f41bc14f87b676551ea66f82b33f17118b0d
Pasha <psmolov@nautilusdigital.com>
2022-12-21 19:01:09 -0500

    chore: convrting errors to responses

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -3,15 +3,17 @@ import { RequestValidationError } from '../errors/request-validation-error'
 import { DatabaseConnectionError } from '../errors/database-connection-error'
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-  if(err instanceof RequestValidationError) {
-    console.log('handling this error as a request validation error')
+  if (err instanceof RequestValidationError) {
+    const formattedErrors = err.errors.map((error) => {
+      return { message: error.msg, field: error.param };
+    });
+    return res.status(400).send({ errors: formattedErrors });
   }
-
-  if(err instanceof DatabaseConnectionError) {
-    console.log('handling this error as a db connection error')
+  if (err instanceof DatabaseConnectionError) {
+    return res.status(500).send({ errors: [{ message: err.reason }] });
   }
 
   res.status(400).send({
-    message: err.message
+    errors: [{ message: 'Something went wrong'}]
   })
 }
