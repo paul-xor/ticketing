8aec648f93315872efbcb47b1bdd1fc7022eac37
Pasha <psmolov@nautilusdigital.com>
2023-01-06 18:51:42 -0500

    chore: add session support

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express, { json } from 'express'
 import 'express-async-errors'
 import mongoose from 'mongoose'
+import cookieSession from 'cookie-session'
 
 import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/singin'
@@ -10,8 +11,15 @@ import { errorHandler } from './middlewares/error-handler'
 import { NotFoundError } from './errors/not-found-error'
 
 const app = express()
+app.set('trust proxy', true)
 mongoose.set('strictQuery', true);
 app.use(json())
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+)
 
 app.use(currentUserRouter)
 app.use(signinRouter)
