681b709ece9a5b0f0a260e6bd6ec806062fa93bd
Pasha <psmolov@nautilusdigital.com>
2023-01-08 14:43:54 -0500

    chore: augmenting type definitions

--- auth/src/middlewares/current-user.ts
+++ auth/src/middlewares/current-user.ts
@@ -1,9 +1,22 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from 'jsonwebtoken';
 
+interface UserPayload {
+  id: string,
+  email: string,
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: UserPayload;
+    }
+  }
+}
+
 export const currentUser = (
   req: Request,
-  res: Response,
+  _res: Response,
   next: NextFunction,
 ) => {
   if (!req.session?.jwt) {
@@ -11,7 +24,7 @@ export const currentUser = (
   }
 
   try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!) as UserPayload;
     req.currentUser = payload;
   } catch (error) {}
   next()
