ebd38de74cd89d51b92eb34e40cdfea8bf37eb13
Pasha <psmolov@nautilusdigital.com>
2023-01-08 18:19:49 -0500

    feat: tests around signin functionality

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -9,7 +9,7 @@ beforeAll(async() => {
 
   const mongo = await MongoMemoryServer.create();
   const mongoUri = mongo.getUri();
-
+  mongoose.set('strictQuery', false)
   await mongoose.connect(mongoUri, {});
 });
 
