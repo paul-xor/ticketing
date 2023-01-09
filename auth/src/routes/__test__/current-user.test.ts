bab701686f880f97a22823bcad1c1c9e3e618eed
Pasha <psmolov@nautilusdigital.com>
2023-01-08 19:42:02 -0500

    feat: testing non-authed request

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -12,3 +12,11 @@ it('responds with details about the current user', async () => {
 
   expect(response.body.currentUser.email).toEqual('test@gmail.com')
 })
+
+it('responds with null if not authenticated', async() => {
+  const response = await request(app)
+    .get('/api/users/currentuser')
+    .send()
+    .expect(200);
+  expect(response.body.currentUser).toEqual(null);
+})
