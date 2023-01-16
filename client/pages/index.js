f523e1fcf3cd110422d14265a9e2ccd71c967297
Pasha <psmolov@nautilusdigital.com>
2023-01-15 21:06:39 -0500

    client: specifying the host

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const LandingPage = ({ currentUser }) => {
-  axios.get('/api/users/currentuser');
   console.log(currentUser);
   // axios.get('/api/users/currentuser').catch((err) => {
   //   console.log(err.message);
@@ -10,10 +9,25 @@ const LandingPage = ({ currentUser }) => {
   return <h1>Landing Page</h1>;
 };
 
-// LandingPage.getInitialProps = async () => {
-//   const response = await axios.get('/api/users/currentuser');
+LandingPage.getInitialProps = async () => {
+  if(typeof window === 'undefined') {
+    // we are on server!
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: {
+          Host: 'ticketing.dev'
+        }
+      }
+    );
+    return data;
+  } else {
+    // we are on the browser!
+    const { data } = await axios.get('/api/users/currentuser');
 
-//   return response.data;
-// }
+    return data;
+  }
+  return {};
+}
 
 export default LandingPage;
