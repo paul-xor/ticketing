3632dd6544ad3b37af35ab2540f93db870a2b55d
Pasha <psmolov@nautilusdigital.com>
2023-01-16 19:36:54 -0500

    client: a reusable api client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser);
@@ -9,23 +9,11 @@ const LandingPage = ({ currentUser }) => {
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  if(typeof window === 'undefined') {
-    // we are on server!
-    const { data } = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-      {
-        headers: req.headers
-      }
-    );
-    return data;
-  } else {
-    // we are on the browser!
-    const { data } = await axios.get('/api/users/currentuser');
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
 
-    return data;
-  }
-  return {};
+  return data;
 }
 
 export default LandingPage;
