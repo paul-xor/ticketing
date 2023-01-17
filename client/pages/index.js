51aef0aca1ebe39f83f3b11e617cedd19892fc74
Pasha <psmolov@nautilusdigital.com>
2023-01-16 19:22:15 -0500

    client: passing through the cookies

--- client/pages/index.js
+++ client/pages/index.js
@@ -9,15 +9,13 @@ const LandingPage = ({ currentUser }) => {
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async () => {
+LandingPage.getInitialProps = async ({ req }) => {
   if(typeof window === 'undefined') {
     // we are on server!
     const { data } = await axios.get(
       'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
       {
-        headers: {
-          Host: 'ticketing.dev'
-        }
+        headers: req.headers
       }
     );
     return data;
