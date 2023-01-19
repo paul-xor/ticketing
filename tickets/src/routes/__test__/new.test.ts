cd1f193306147c2d5ef8276a83006f6195d61bab
Pasha <psmolov@nautilusdigital.com>
2023-01-18 20:30:48 -0500

    tickets: creation via route handler

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
 
 it('has a route handler listening to /api/tickets for post requests' , async () => {
   const response = await request(app)
@@ -60,11 +61,18 @@ it('returns an error is an invalid price is provided' , async () => {
     .expect(400);
 })
 it('creates a ticket with valid inputs' , async () => {
+  let tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(0);
+
   await request(app)
     .post('/api/tickets')
+    .set('Cookie', global.signin())
     .send({
       title: 'asjdfljdf',
       price: 30,
     })
     .expect(201);
+
+  tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(1);
 })
