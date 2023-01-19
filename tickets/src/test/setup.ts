ea7257448c899d5399368f4ce87f0786ce422325
Pasha <psmolov@nautilusdigital.com>
2023-01-18 19:34:14 -0500

    tickets: building session

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -2,16 +2,10 @@ import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import { app } from '../app';
 import request from 'supertest';
+import jwt from 'jsonwebtoken';
 
-// declare global {
-//   namespace NodeJS {
-//     export interface Global {
-//       signin(): Promise<string[]>
-//     }
-//   }
-// }
 declare global {
-  var signin: () => Promise<string[]>;
+  var signin: () => string[];
 }
 
 let mongo: any;
@@ -40,17 +34,25 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signin = async () => {
+global.signin = () => {
   // build jsonwebtoken payload {id, email}
+  const payload = {
+    id: '2kou32kj42k',
+    email: 'test@test.com'
+  }
 
   // Create the JWT!
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
 
   // Build a session Object { jwt: MY_JWT }
+  const session = { jwt: token };
 
   // Turn this session into JSON
+  const sessionJSON = JSON.stringify(session);
 
   // Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString('base64');
 
   // return a string thats the cookie with the encoded data
-
+  return [`session=${base64}`];
 };
