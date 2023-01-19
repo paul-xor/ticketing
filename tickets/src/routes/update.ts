c381d8d944799f697edd7452e40fc9dffb0bb5ed
Pasha <psmolov@nautilusdigital.com>
2023-01-19 18:19:30 -0500

    tickets: final update changes

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -14,6 +14,16 @@ const router = express.Router();
 router.put(
   '/api/tickets/:id',
   requireAuth,
+  [
+    body('title')
+      .not()
+      .isEmpty()
+      .withMessage('Title is required'),
+    body('price')
+      .isFloat({ gt: 0 })
+      .withMessage('Price must be provided and be greater than 0')
+  ],
+  validateRequest,
   async (req: Request, res: Response) => {
     const ticket = await Ticket.findById(req.params.id);
 
@@ -24,6 +34,12 @@ router.put(
     if (ticket.userId !== req.currentUser!.id) {
       throw new NotAuthorizedError();
     }
+
+    ticket.set({
+      title: req.body.title,
+      price: req.body.price
+    });
+    await ticket.save();
 
     res.send(ticket);
   }
