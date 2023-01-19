cf27904849899f858677f6c464a7511821fc7b3f
Pasha <psmolov@nautilusdigital.com>
2023-01-18 19:51:54 -0500

    tickets: validating title and price

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,9 +1,18 @@
 import express, { Request, Response } from 'express';
-import { requireAuth } from '@small-tickets/common';
+import { body } from 'express-validator';
+import { requireAuth, validateRequest } from '@small-tickets/common';
 
 const router = express.Router();
 
-router.post('/api/tickets', requireAuth, (req: Request, res: Response) => {
+router.post('/api/tickets', requireAuth, [
+  body('title')
+    .not()
+    .isEmpty()
+    .withMessage('Title is required'),
+  body('price')
+    .isFloat({ gt: 0 })
+    .withMessage('Price must be greater then 0')
+], validateRequest, (req: Request, res: Response) => {
   res.sendStatus(200);
 });
 
