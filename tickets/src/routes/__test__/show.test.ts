f95ba5046bb90e948d7bc199d3f5858fb83ad032
Pasha <psmolov@nautilusdigital.com>
2023-01-19 16:44:25 -0500

    tickets: better error loging

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,13 +1,15 @@
 import request from 'supertest';
 import { app } from '../../app';
+import mongoose from 'mongoose';
 
 
 it('returns a 404 if the ticket is not found', async() => {
+  const id = new mongoose.Types.ObjectId().toHexString();
   await request(app)
-    .get('/api/tickets/sdfjldskjf')
+    .get(`/api/tickets/${id}`)
     .send()
     .expect(404);
-})
+});
 it('returns the ticket is found', async () => {
   const title = 'concert';
   const price = 20;
