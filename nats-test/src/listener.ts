609e969da94e38167eda1e46d75aa592da71f72d
Pasha <psmolov@nautilusdigital.com>
2023-01-21 18:59:34 -0500

    nats-test: extending the listener

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,11 +1,10 @@
 import nats, { Message, Stan } from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
-import { StandardSanitizer } from 'express-validator/src/base';
 
 console.clear();
 
 const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
-  url: 'http://localhost:4222'
+  url: 'http://localhost:4222',
 });
 
 stan.on('connect', () => {
@@ -14,29 +13,9 @@ stan.on('connect', () => {
   stan.on('close', () => {
     console.log('NATS connection closed!');
     process.exit();
-  })
+  });
 
-  const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable()
-    .setDurableName('acount-service');
-
-  const subscription = stan.subscribe(
-    'ticket:created',
-    'orders-service-queue-group',
-    options
-  );
-
-  subscription.on('message', (msg: Message) => {
-    const data = msg.getData();
-
-    if (typeof data === 'string') {
-      console.log(`Recieved event #${msg.getSequence()}, with data: ${data}`);
-    }
-
-    msg.ack();
-  })
+  new TicketCreatedListener(stan).listen();
 });
 
 process.on('SIGINT', () => stan.close());
@@ -69,19 +48,29 @@ abstract class Listener {
       this.subscriptionOptions()
     );
 
-    subscription.on('message', (msg) => {
-      console.log(`Message recieved: ${this.subject}/ ${this.queueGroupName}`)
+    subscription.on('message', (msg: Message) => {
+      console.log(`Message received: ${this.subject} / ${this.queueGroupName}`);
 
       const parsedData = this.parseMessage(msg);
       this.onMessage(parsedData, msg);
-    })
+    });
   }
 
   parseMessage(msg: Message) {
     const data = msg.getData();
     return typeof data === 'string'
       ? JSON.parse(data)
-      : JSON.parse(data.toString('utf-8'));
+      : JSON.parse(data.toString('utf8'));
+  }
+}
 
+class TicketCreatedListener extends Listener {
+  subject = 'ticket:created';
+  queueGroupName = 'payments-service';
+
+  onMessage(data: any, msg: Message) {
+    console.log('Event data!', data);
+
+    msg.ack();
   }
 }
