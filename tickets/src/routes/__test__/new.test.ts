d6c9ad0f29aa4d167ace0bf8569285b3cfad8029
Pasha <psmolov@nautilusdigital.com>
2023-01-23 18:36:24 -0500

    tickets: ensuring mock invocations

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import { app } from '../../app';
 import { Ticket } from '../../models/ticket';
+import { natsWrapper } from '../../nats-wrapper';
 
 it('has a route handler listening to /api/tickets for post requests' , async () => {
   const response = await request(app)
@@ -64,15 +65,35 @@ it('creates a ticket with valid inputs' , async () => {
   let tickets = await Ticket.find({});
   expect(tickets.length).toEqual(0);
 
+  const title = 'asdjfl';
+
   await request(app)
     .post('/api/tickets')
     .set('Cookie', global.signin())
     .send({
-      title: 'asjdfljdf',
+      title,
       price: 30,
     })
     .expect(201);
 
   tickets = await Ticket.find({});
   expect(tickets.length).toEqual(1);
+  expect(tickets[0].title).toEqual(title);
+  expect(tickets[0].price).toEqual(30);
+})
+
+it('publishes an event', async () => {
+  const title = 'asdjfl';
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title,
+      price: 30,
+    })
+    .expect(201);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+
 })
