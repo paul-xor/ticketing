08459855ccf693d2a3ea9de116293b78bb204b04
Pasha <psmolov@nautilusdigital.com>
2023-01-23 20:20:56 -0500

    orders: subtitle service coupling

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,8 +1,18 @@
+import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
+import { requireAuth, validateRequest } from '@small-tickets/common';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/orders', (req: Request, res: Response) => {
+router.post('/api/orders', requireAuth, [
+  body('ticketId')
+    .not()
+    .isEmpty()
+    // below custom checks if id is a mongodb id it could be deleted in case of other db to be used
+    .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+    .withMessage('TicketId must be provided')
+], validateRequest, async (req: Request, res: Response) => {
   res.send({});
 });
 
