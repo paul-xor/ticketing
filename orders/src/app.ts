1561e5312aab68a0d2f1716b219cf0642c5804bc
Pasha <psmolov@nautilusdigital.com>
2023-01-23 20:08:42 -0500

    orders: scaffolding a few route handlers

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -2,10 +2,10 @@ import express, { json } from 'express';
 import 'express-async-errors';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@small-tickets/common';
-import { createTicketRouter } from './routes/new';
-import { showTicketRouter } from './routes/show';
-import { indexTicketRouter } from './routes/index';
-import { updateTicketRouter } from './routes/update';
+import { deleteOrderRouter } from './routes/delete';
+import { indexOrderRouter } from './routes';
+import { newOrderRouter } from './routes/new';
+import { showOrderRouter } from './routes/show';
 
 const app = express();
 app.set('trust proxy', true);
@@ -18,10 +18,10 @@ app.use(
 );
 
 app.use(currentUser);
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
+app.use(deleteOrderRouter);
+app.use(showOrderRouter);
+app.use(newOrderRouter);
+app.use(indexOrderRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
