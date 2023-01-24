609dcbe0c153481261ee6288b65da59132fd9ab9
Pasha <psmolov@nautilusdigital.com>
2023-01-24 16:50:32 -0500

    orders: finding reserved ticket

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,6 +1,11 @@
 import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
-import { NotFoundError, requireAuth, validateRequest } from '@small-tickets/common';
+import {
+  NotFoundError,
+  BadRequestError,
+  requireAuth,
+  validateRequest,
+  OrderStatus } from '@small-tickets/common';
 import { body } from 'express-validator';
 import { Ticket } from '../models/ticket';
 import { Order } from '../models/order';
@@ -23,7 +28,22 @@ router.post('/api/orders', requireAuth, [
   }
 
   // Make sure this ticket is not already reserved
-
+  // Run query to look at all orders. Find an order where the ticket
+  // is the ticket we just found *and* the order status is *not* cancelled.
+  // if we find an order from that means the ticket *is* reserved
+  const existingOrder = await Order.findOne({
+    ticket: ticket,
+    status: {
+      $in: [
+        OrderStatus.Created,
+        OrderStatus.AwaitingPayment,
+        OrderStatus.Complete
+      ]
+    }
+  });
+  if(existingOrder) {
+    throw new  BadRequestError('Ticket is already reserved.');
+  }
   // Caoculate an expiration date for this order
 
   // build the order and save to db.
