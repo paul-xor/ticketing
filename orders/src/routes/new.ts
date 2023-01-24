bc225e05e2b099ac4c984802f47b02dbf3366d89
Pasha <psmolov@nautilusdigital.com>
2023-01-24 16:26:00 -0500

    orders: order creation logic

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
-import { requireAuth, validateRequest } from '@small-tickets/common';
+import { NotFoundError, requireAuth, validateRequest } from '@small-tickets/common';
 import { body } from 'express-validator';
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
@@ -13,6 +15,21 @@ router.post('/api/orders', requireAuth, [
     .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
     .withMessage('TicketId must be provided')
 ], validateRequest, async (req: Request, res: Response) => {
+  const { ticketId } = req.body;
+  // Find the ticket the user is tring to order in the db
+  const ticket = await Ticket.findById(ticketId);
+  if (!ticket) {
+    throw new NotFoundError();
+  }
+
+  // Make sure this ticket is not already reserved
+
+  // Caoculate an expiration date for this order
+
+  // build the order and save to db.
+
+  // Publish an event saying that an order was created.
+
   res.send({});
 });
 
