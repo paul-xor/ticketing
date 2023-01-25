08863ede3ae1558c4fb59505703ef6f8d29f2a14
Pasha <psmolov@nautilusdigital.com>
2023-01-24 20:28:35 -0500

    orders: cancelling an order and test

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,23 @@
 import express, { Request, Response } from 'express';
+import { Order } from '../models/order';
+import { OrderStatus, requireAuth, NotFoundError, NotAuthorizedError } from '@small-tickets/common';
 
 const router = express.Router();
 
-router.delete('/api/orders/:orderId', (req: Request, res: Response) => {
-  res.send({});
+router.delete('/api/orders/:orderId', requireAuth, async (req: Request, res: Response) => {
+  const { orderId } = req.params;
+
+  const order = await Order.findById(orderId);
+  if (!order) {
+    throw new NotFoundError();
+  }
+  if(order.userId !== req.currentUser!.id) {
+    throw new NotAuthorizedError();
+  }
+  order.status = OrderStatus.Cancelled;
+  await order.save();
+
+  res.status(204).send(order);
 });
 
 export { router as deleteOrderRouter };
