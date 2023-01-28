cd7115a6653f1fbd0a84e6f755c9f7df581d4a35
Pasha <psmolov@nautilusdigital.com>
2023-01-27 21:27:29 -0500

    tickets: applying a version query

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -51,14 +51,15 @@ router.post('/api/orders', requireAuth, [
   // Publish an event saying that an order was created.
   new OrderCreatedPublisher(natsWrapper.client).publish({
     id: order.id,
+    version: order.version,
     status: order.status,
     userId: order.userId,
     expiresAt: order.expiresAt.toISOString(),
     ticket: {
       id: ticket.id,
       price: ticket.price,
-    }
-  })
+    },
+  });
 
   res.status(201).send(order);
 });
