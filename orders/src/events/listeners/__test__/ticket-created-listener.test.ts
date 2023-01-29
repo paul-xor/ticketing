6a214f9907cdea953f43035b8f7eecaefdc0fa6c
Pasha <psmolov@nautilusdigital.com>
2023-01-28 19:45:12 -0500

    orders: testing listeners

---
+++ orders/src/events/listeners/__test__/ticket-created-listener.test.ts
@@ -0,0 +1,22 @@
+
+
+const setup = async () => {
+  // create an instance of the listener
+
+  // create a fake data event
+
+  // create a fake message object
+}
+
+it('creates an saves a ticket', async() => {
+  // call the onMessage function with the data object + message object
+
+  // write assertions to make sure a ticket was created!
+
+})
+it('acks the message', async() => {
+  // call the onMessage function with the data object + message object
+
+  // write assertions to make sure a ack is called!
+
+})
