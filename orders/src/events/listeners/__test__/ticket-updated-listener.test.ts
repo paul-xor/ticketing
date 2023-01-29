e213a04a7f4fc2d8d0efa5df915e394832c1e420
Pasha <psmolov@nautilusdigital.com>
2023-01-28 20:36:58 -0500

    orders: success case testing

--- orders/src/events/listeners/__test__/ticket-updated-listener.test.ts
+++ orders/src/events/listeners/__test__/ticket-updated-listener.test.ts
@@ -36,5 +36,19 @@ const setup = async() => {
   return { msg, data, ticket, listener };
 }
 
-it('finds, updates and saves a ticket', async() => {})
-it('acks the message', async() => {})
+it('finds, updates and saves a ticket', async() => {
+  const { msg, data, ticket, listener } = await setup();
+
+  await listener.onMessage(data, msg);
+  const updatedTicket = await Ticket.findById(ticket.id);
+
+  expect(updatedTicket!.title).toEqual(data.title);
+  expect(updatedTicket!.price).toEqual(data.price);
+  expect(updatedTicket!.version).toEqual(data.version);
+ })
+it('acks the message', async() => {
+  const { msg, data, listener } = await setup();
+
+  await listener.onMessage(data, msg);
+  expect(msg.ack).toHaveBeenCalled();
+})
