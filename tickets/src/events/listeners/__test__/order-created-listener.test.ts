b6118142945bbd063bf5c5ab8f423debec7c97d5
Pasha <psmolov@nautilusdigital.com>
2023-01-29 20:34:35 -0500

    tickets: publishing while listening

--- tickets/src/events/listeners/__test__/order-created-listener.test.ts
+++ tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import { Message } from "node-nats-streaming";
 import { OrderCreatedEvent, OrderStatus } from "@small-tickets/common";
-import { OrderCreatedListener } from "../order-create-listener"
+import { OrderCreatedListener } from "../order-created-listener"
 import { natsWrapper } from "../../../nats-wrapper"
 import { Ticket } from "../../../models/ticket";
 
--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -4,11 +4,11 @@ import {
 import { Message } from "node-nats-streaming";
 import { queueGroupName } from "./queue-group-name";
 import { Ticket } from "../../models/ticket";
+import { TicketUpdatedPublisher } from '../publishers/ticket-updated-publisher';
 
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
-
   queueGroupName = queueGroupName;
 
   async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
@@ -22,6 +22,13 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     ticket.set({ orderId: data.id });
     // Save the ticket
     await ticket.save();
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      version: ticket.version,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     // ack the message
     msg.ack();
