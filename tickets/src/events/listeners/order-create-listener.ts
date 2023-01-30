9b28000f81c5a00d5eb0685f38829e80e61e5aba
Pasha <psmolov@nautilusdigital.com>
2023-01-29 19:30:43 -0500

    tickets: reserving a ticket

--- tickets/src/events/listeners/order-create-listener.ts
+++ tickets/src/events/listeners/order-create-listener.ts
@@ -1,6 +1,9 @@
-import { Listener, OrderCreatedEvent, Subjects } from "@small-tickets/common";
+import {
+  Listener, OrderCreatedEvent, Subjects
+} from "@small-tickets/common";
 import { Message } from "node-nats-streaming";
 import { queueGroupName } from "./queue-group-name";
+import { Ticket } from "../../models/ticket";
 
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
@@ -8,5 +11,19 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 
   queueGroupName = queueGroupName;
 
-  onMessage(data: OrderCreatedEvent['data'], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    // Find the ticket that the order is reserved
+    const ticket = await Ticket.findById(data.ticket.id);
+    // If no ticket, throw an error
+    if (!ticket) {
+      throw new Error('Ticket NOT found');
+    }
+    // Mark the ticket as being reserved by setting its orderId property
+    ticket.set({ orderId: data.id });
+    // Save the ticket
+    await ticket.save();
+
+    // ack the message
+    msg.ack();
+  }
 }
