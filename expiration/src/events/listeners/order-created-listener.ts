fc9d45585c33193c26efff2a02b76fd16552c18a
Pasha <psmolov@nautilusdigital.com>
2023-01-30 20:24:47 -0500

    expiration: listener creation

---
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,13 @@
+import { Listener, OrderCreatedEvent, OrderStatus, Subjects } from "@small-tickets/common";
+import { Message } from "node-nats-streaming";
+import { queueGroupName } from "./queue-group-name";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject: Subjects.OrderCreated = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+
+  }
+
+}
