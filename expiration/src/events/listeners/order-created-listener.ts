2e2b593028d115450de3e2134ea7e974f360ac16
Pasha <psmolov@nautilusdigital.com>
2023-01-31 20:07:25 -0500

    expiration: queueing a job on event arrival

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,13 +1,16 @@
 import { Listener, OrderCreatedEvent, OrderStatus, Subjects } from "@small-tickets/common";
 import { Message } from "node-nats-streaming";
 import { queueGroupName } from "./queue-group-name";
+import { expirationQueue } from "../../queues/expiration-queue";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
 
   async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
-
+    await expirationQueue.add({
+      orderId: data.id
+    });
+    msg.ack();
   }
-
 }
