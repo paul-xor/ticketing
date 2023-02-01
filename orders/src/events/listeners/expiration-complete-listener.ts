87bf6ab4d3a8e1defd504061b8d0a70d7a916d64
Pasha <psmolov@nautilusdigital.com>
2023-01-31 21:14:13 -0500

    orders: handling an expiration event

---
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -0,0 +1,22 @@
+import { Listener, Subjects, ExpirationCompleteEvent, OrderStatus } from '@small-tickets/common';
+import { Message } from 'node-nats-streaming';
+import { queueGroupName } from './queue-group-name';
+import { Order } from '../../models/order';
+
+export class ExprirationCompleteListener extends Listener<ExpirationCompleteEvent> {
+  subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
+    const order = await Order.findById(data.orderId);
+
+    if (!order) {
+      throw new Error ('Order not found')
+    }
+
+    order.set({
+      status: OrderStatus.Cancelled,
+      ticket: null,
+    });
+  }
+}
