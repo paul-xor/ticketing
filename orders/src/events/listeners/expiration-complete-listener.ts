1ea16880695a1333fce84b28c234ec146ce61b69
Pasha <psmolov@nautilusdigital.com>
2023-01-31 21:23:56 -0500

    orders: emitting the otder cancelled event

--- orders/src/events/listeners/expiration-complete-listener.ts
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -2,13 +2,14 @@ import { Listener, Subjects, ExpirationCompleteEvent, OrderStatus } from '@small
 import { Message } from 'node-nats-streaming';
 import { queueGroupName } from './queue-group-name';
 import { Order } from '../../models/order';
+import { OrderCancelledPublisher } from '../publishers/order-cancelled-publisher';
 
 export class ExprirationCompleteListener extends Listener<ExpirationCompleteEvent> {
   subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
   queueGroupName = queueGroupName;
 
   async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
-    const order = await Order.findById(data.orderId);
+    const order = await Order.findById(data.orderId).populate('ticket');
 
     if (!order) {
       throw new Error ('Order not found')
@@ -16,7 +17,17 @@ export class ExprirationCompleteListener extends Listener<ExpirationCompleteEven
 
     order.set({
       status: OrderStatus.Cancelled,
-      ticket: null,
     });
+
+    await order.save();
+    new OrderCancelledPublisher(this.client).publish({
+      id: order.id,
+      version: order.version,
+      ticket: {
+        id: order.ticket.id,
+      }
+    });
+
+    msg.ack();
   }
 }
