dcd2ee462dad1e8c96063973e4139365dba63a91
Pasha <psmolov@nautilusdigital.com>
2023-02-02 20:07:16 -0500

    payments: update-if-current

--- payments/src/models/orders.ts
+++ payments/src/models/orders.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import { OrderStatus } from '@small-tickets/common';
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
 
 interface OrderAttrs {
   id: string,
@@ -43,6 +44,9 @@ const orderSchema = new mongoose.Schema({
     }
   }
 });
+
+orderSchema.set('versionKey', 'version');
+orderSchema.plugin(updateIfCurrentPlugin);
 
 orderSchema.statics.build = (attrs: OrderAttrs) => {
   return new Order({
