87abe12cfa0d1dfd757100db3b056fb1d1e23f4d
Pasha <psmolov@nautilusdigital.com>
2023-02-06 21:17:45 -0500

    client: sanitazing price input

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -1,15 +1,39 @@
+import { useState } from "react";
+
 const NewTicket = () => {
+  const [title, setTitle] = useState('');
+  const [price, setPrice] = useState('');
+
+  const onBlur = () => {
+    const value = parseFloat(price);
+
+    if (isNaN(value)) {
+      return;
+    }
+
+    setPrice(value.toFixed(2));
+  }
+
   return (
   <div>
     <h1>Create a Ticket</h1>
     <form>
       <div className="form-group">
         <label>Title</label>
-        <input className="form-control" />
+        <input
+          className="form-control"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
       </div>
         <div className="form-group" style={{ margin: "15px 0" }}>
         <label>Price</label>
-        <input className="form-control" />
+        <input
+          className="form-control"
+          value={price}
+          onBlur={onBlur}
+          onChange={(e) => setPrice(e.target.value)}
+        />
       </div>
     </form>
     <button className="btn btn-primary">Submit</button>
