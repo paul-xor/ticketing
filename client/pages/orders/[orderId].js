43d2db51f631b016532eb8ac2798e571a49b16a0
Pasha <psmolov@nautilusdigital.com>
2023-02-08 16:05:16 -0500

    client: test credit card numbers

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -27,7 +27,7 @@ const OrderShow = ({ order, currentUser }) => {
       Time left to pay: {timeLeft} seconds
       <StripeCheckout
         token={(token) => console.log(token)}
-        stripeKey={process.env.STRIPE_PUB_KEY}
+        stripeKey={process.env.NEXT_PUBLIC_STRIPE_PUB_KEY}
         amount={order.ticket.price * 100}
         email={currentUser.email}
       />
